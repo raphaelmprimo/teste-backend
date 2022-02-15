f01fb4b816df0c3b8f723a6f533906c11488e914
raphaelmprimo <raphael_primo@live.com>
2022-02-15 10:05:45 -0300

    Creation of the table migrations

---
+++ link-shortener/database/migrations/2022_02_15_125437_create_access_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAccessTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('access', function (Blueprint $table) {
+            $table->id();
+            $table->integer('link_id');
+            $table->string('ip');
+            $table->string('user_agent');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('access');
+    }
+}

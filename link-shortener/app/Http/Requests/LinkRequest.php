143fa053b875bc636f444771fbb513c72d7eb4d1
raphaelmprimo <raphael_primo@live.com>
2022-02-15 11:01:58 -0300

    Creation of link request

---
+++ link-shortener/app/Http/Requests/LinkRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class LinkRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'url' => 'bail|required|url|string|max:255',
+            'slug' => 'bail|nullable|string|min:6|max:8|unique:links',
+        ];
+    }
+}
